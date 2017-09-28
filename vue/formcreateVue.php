0bbc11993214df411be82a2a659b2be69de9f3bc
Sonia Chemaa <sonia.chemaa@outlook.com>
2017-09-28 11:07:35 +0200

    html form create category 'project'

---
+++ vue/formcreateVue.php
@@ -0,0 +1,17 @@
+<?php include("template/header.php"); ?>
+
+<form class="" action="" method="post">
+  
+<p>Title:</p>
+<input type="text" name="title" value="">
+<p>Status:</p>
+<input type="radio" name="status" value="ongoing" id="ongoing"/><label for="ongoing">On Going</label>
+<input type="radio" name="status" value="finished" id="finished"/><label for="finished">Finished</label>
+<p>Description:</p>
+<input type="text" name="description" value="">
+<input type="submit" name="send" value="Create Project">
+
+</form>
+
+
+<?php include("template/footer.php"); ?>
