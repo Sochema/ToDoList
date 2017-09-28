d21807590777455f159b0425dad168fab53c4f2c
Sonia Chemaa <sonia.chemaa@outlook.com>
2017-09-28 12:09:38 +0200

    file connection moved

--- modele/insertproject.php
+++ modele/insertproject.php
@@ -2,9 +2,12 @@
 
 require_once"connection_sql.php";
 
-function insertProject(){
-  $req = get_db() -> prepare('INSERT INTO project ')
+function insertProject($title, $status, $description){
+  $req = get_db() -> prepare('INSERT INTO project VALUES (:title, :status, :description, CURDATE())');
+  $req -> execute(array(
+    'title' => $title,
+    'status' => $status,
+    'description' => $description
+  ));
 
-
-  
 }
